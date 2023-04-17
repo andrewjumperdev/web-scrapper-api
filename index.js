1986122a1f0dddd54c85075982120bc64f434565
andrewjumperdev <andrew.jumper.ent@gmail.com>
2023-04-17 16:52:59 +0200

    first commit

--- index.js
+++ index.js
@@ -1,60 +1,42 @@
-const PORT = 8080
-const axios = require('axios');
-const cheerio = require('cheerio');
-const express = require('express');
+const cheerio = require("cheerio");
+const request = require("request-promise");
 
-const app = express()
+const url = "https://bobochicparis.com/fr/plan-du-site";
 
-const urlBut = 'https://www.but.fr/collections/bobochic/index-c10350.html'
+async function init() {
+  
+  const links = []
 
-const urlHome24 = 'https://www.home24.fr/search?query=bobochic'
+  const $ = await request({
+    uri: url,
+    transform: (body) => cheerio.load(body),
+  });
 
-const urlGreenweez = 'https://www.greenweez.com/marque/bobochic'
+  $('a').each(function(i) {
+    links[i] = $(this).attr('href')
+  })
 
-axios(urlBut)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.gtm-product', html).each(function() {
-            const title = $(this).text()
-            const description = $(this).children('div:last').text()
-            articles.push({
-                title,
-                description,
-            })
-        })         
-        console.log(articles)       
-    }).catch(err => console.log(err))
+  links.map((item) => {
+      if(/(http|https):\/\/([^\/\r\n]+)(\/[^\r\n]*)?/.test(item)) {
+        console.log(item)
+      } else {
+        console.log(item)
+      }
+  })
+}
 
-    axios(urlHome24)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const products = []
-        $('.css-xkjszp', html).each(function() {
-            const title = $(this).text()
-            const url = `https://www.home24.fr/${$(this).attr('href')}`
-            products.push({
-                title,
-                url,
-            })
-        })         
-        console.log(products)       
-    }).catch(err => console.log(err))
+function validarURL(miurl) {
+  try {
+ 
+    new URL(miurl);
+    return true;
+ 
+  } catch (err) {
+ 
+    return false;
+ 
+  }
+}
+ 
+init()
 
-    axios(urlGreenweez)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const products = []
-        $('.gds-product-card-title__wrapper', html).each(function() {
-            const title = $(this).children('.gds-product-card-title__title').text()
-            products.push({
-                title,
-            })
-        })         
-        console.log(products)       
-    }).catch(err => console.log(err))
-
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
